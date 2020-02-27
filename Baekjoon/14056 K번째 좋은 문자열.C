ca71d3903535beb55cdc7e5ec8c07bf92c79436d
Freeprocessor <wmlee97@naver.com>
2020-02-27 16:10:42 +0900

    commit

---
+++ Baekjoon/14056 K번째 좋은 문자열.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char* argv[])
+{
+    int getn=0;
+    getn=getchar();
+}
+
+int replacement(int n, char *gondolaSeq, char *replacementSeq)
+{
+    int i=0;
+    //replacementSeq[i] Test 2dsf dsfds
+}
